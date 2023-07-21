8dad8f3ae7ee338d42f64546fddcfb02483ae8c2
20A3020 <139091135+20A3020@users.noreply.github.com>
2023-07-21 13:17:31 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+
+import streamlit as st
+import openai
+
+# Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
+openai.api_key = st.secrets.OpenAIAPI.openai_api_key
+
+# st.session_stateを使いメッセージのやりとりを保存
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "system", "content": "st.secrets.AppSettings.appset"}
+        ]
+
+# チャットボットとやりとりする関数
+def communicate():
+    messages = st.session_state["messages"]
+
+    user_message = {"role": "user", "content": st.session_state["user_input"]}
+    messages.append(user_message)
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        temperature=2,
+        messages=messages
+    )
+
+    bot_message = response["choices"][0]["message"]
+    messages.append(bot_message)
+
+    st.session_state["user_input"] = ""  # 入力欄を消去
+
+
+# ユーザーインターフェイスの構築
+st.title("卒論の校正はお任せ！")
+st.write("ChatGPT APIを使ってあなたの卒論を校正します。")
+
+user_input = st.text_input("文章を入力", key="user_input", on_change=communicate)
+
+if st.session_state["messages"]:
+    messages = st.session_state["messages"]
+
+    for message in reversed(messages[1:]):  # 直近のメッセージを上に
+        speaker = "👦あなたのテキスト"
+        if message["role"]=="assistant":
+            speaker="🤖校正されたテキスト"
+
+        st.write(speaker + ": " + message["content"])
