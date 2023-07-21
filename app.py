84708e684ede41fc72a6eaed0ee66f8897ed3927
20A3020 <139091135+20A3020@users.noreply.github.com>
2023-07-21 19:51:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -30,10 +30,10 @@ def communicate():
 
 
 # ユーザーインターフェイスの構築
-st.title("卒論の校正はお任せ！")
+st.title("校正はお任せ！")
 st.write("ChatGPT APIを使ってあなたの卒論を校正します。")
 
-user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
+user_input = st.text_input("校正したい文章を入力してください。", key="user_input", on_change=communicate)
 
 if st.session_state["messages"]:
     messages = st.session_state["messages"]
